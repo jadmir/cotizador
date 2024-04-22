3321780223392c96456226b931ad1965be8a7bd2
jadmir <jadmir1111@gmail.com>
2024-04-22 12:37:39 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import Header from "./components/Header"
+function App() {
+
+  const [cantidad, setCantidad] = useState(10000);
+
+  console.log(cantidad)
+
+  return (
+    <div className="my-20 max-w-lg mx-auto bg-white shadow p-10">
+      <Header />
+
+      <input 
+        type="range" 
+        className="w-full h-6 bg-gray-200 accent-lime-500 hover:accent-lime-600"
+      />
+    </div>
+  )
+}
+
+export default App
