684d1802ef2c2f7d0d7bc1854b138f60e789c0fd
JADMIR IGNACIO VICENTE SIFUENTES <jvicentes@utp.edu.pe>
2024-04-22 21:56:12 -0500

    botones

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,67 @@
 import { useState } from 'react'
 import Header from "./components/Header"
+import Button from "./components/Button"
+import { formatearDinero } from './helpers'
 function App() {
 
   const [cantidad, setCantidad] = useState(10000);
 
-  console.log(cantidad)
+  const MIN = 0
+  const MAX = 20000
+  const STEP = 100
+
+  function handleChange(e) {
+    setCantidad(+e.target.value)
+  }
+
+  function handleClickDecremento() {
+    const valor = cantidad - STEP
+
+    if(valor < MIN) {
+      alert('cantidad no valida')
+      return
+    }
+    setCantidad(valor)
+  }
+
+  function handleClickIncremento() {
+    const valor = cantidad + STEP
+
+    if(valor > MAX) {
+      alert('Exediste el valor permitido')
+      return
+    }
+    setCantidad(valor)
+  }
 
   return (
     <div className="my-20 max-w-lg mx-auto bg-white shadow p-10">
       <Header />
 
+      <div className='flex justify-between my-6'>
+        <Button 
+          operador='-'
+          fn={handleClickDecremento}
+        />
+        <Button 
+          operador='+'
+          fn={handleClickIncremento}
+        />
+      </div>
+      
       <input 
         type="range" 
         className="w-full h-6 bg-gray-200 accent-lime-500 hover:accent-lime-600"
+        onChange={handleChange}
+        min={MIN}
+        max={MAX}
+        step={STEP}
+        value={cantidad}
       />
+
+      <p className='text-center my-10 text-5xl font-extrabold text-indigo-600'>
+        {formatearDinero(cantidad)}
+      </p>
     </div>
   )
 }
